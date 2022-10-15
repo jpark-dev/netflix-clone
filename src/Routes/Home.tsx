77cc43801cb76e6deb6e1a1dc6c0ee416216d7d4
Jason Park <zeipar@gmail.com>
2022-10-15 13:04:58 -0700

    Linted.

--- src/Routes/Home.tsx
+++ src/Routes/Home.tsx
@@ -1,29 +1,13 @@
 import { useQuery } from "react-query";
-import { getMovies } from "../api";
-
-interface Movie {
-  id: number;
-  backdrop_path: string;
-  poster_path: string;
-  title: string;
-  overview: string;
-}
-export interface MoviesResult {
-  dates: {
-    minimum: string;
-    maximum: string;
-  },
-  page: number;
-  results: Movie[];
-  total_pages: number;
-  total_results: number;
-}
+import { getMovies, MoviesResult } from "../api";
 
 function Home() {
-  const { data, isLoading, } = useQuery<MoviesResult>(["movies", "nowPlaying"], getMovies);
-  console.log('data', data, isLoading);
+  const { data, isLoading } = useQuery<MoviesResult>(
+    ["movies", "nowPlaying"],
+    getMovies
+  );
 
-  return <div style={{ backgroundColor: "", height: "200vh"}}></div>;
+  return <div style={{ backgroundColor: "", height: "200vh" }}></div>;
 }
 
 export default Home;
--- src/api.ts
+++ src/api.ts
@@ -1,6 +1,27 @@
-const BASE_URL = "https://api.themoviedb.org/3/movie/now_playing?api_key=0cad3c39c392fdd7603017a627b29b50&language=en-US&page=1";
+const BASE_URL =
+  "https://api.themoviedb.org/3/movie/now_playing?api_key=0cad3c39c392fdd7603017a627b29b50&language=en-US&page=1";
 const API_KEY = process.env.REACT_APP_MOVIE_API_KEY;
 
+interface Movie {
+  id: number;
+  backdrop_path: string;
+  poster_path: string;
+  title: string;
+  overview: string;
+}
+export interface MoviesResult {
+  dates: {
+    minimum: string;
+    maximum: string;
+  };
+  page: number;
+  results: Movie[];
+  total_pages: number;
+  total_results: number;
+}
+
 export function getMovies() {
-  return fetch(`${BASE_URL}/movie/now_playing?api_key${API_KEY}`).then(res => res.json());
+  return fetch(`${BASE_URL}/movie/now_playing?api_key${API_KEY}`).then((res) =>
+    res.json()
+  );
 }
